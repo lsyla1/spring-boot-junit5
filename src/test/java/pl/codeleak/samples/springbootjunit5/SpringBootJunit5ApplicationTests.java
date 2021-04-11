014c831465850498b56e1b687ded9803ee80564d
lsyla1 <liridonsyla1000@gmail.com>
2021-04-11 18:53:23 +0200

    add docker

--- src/test/java/pl/codeleak/samples/springbootjunit5/SpringBootJunit5ApplicationTests.java
+++ src/test/java/pl/codeleak/samples/springbootjunit5/SpringBootJunit5ApplicationTests.java
@@ -1,11 +1,21 @@
 package pl.codeleak.samples.springbootjunit5;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootTest
 class SpringBootJunit5ApplicationTests {
-
+    @Autowired
+    private MeterRegistry meterRegistry;
+    private final AtomicInteger testGauge;
+    private final Counter testCounter;
+    testGauge = meterRegistry.gauge("custom_gauge", new AtomicInteger(0));
+   testCounter = meterRegistry.counter("custom_counter");
     @Test
     void contextLoads() {
     }
